0dc4cb4142c9625e292ad718d4f3bd272c065a92
Jett Moy <jettmoy@gmail.com>
2018-11-29 19:33:35 -0800

    dkl;fajkd

--- DiGraph.java
+++ DiGraph.java
@@ -5,45 +5,51 @@
 * Part 1 & 2
 */
 
+import java.util.LinkedList;
+import java.util.ArrayList;
+
 public class DiGraph {
-    private LinkedList<Integer>[] graph;
+    private ArrayList<LinkedList> graph;
 
     public DiGraph(int N) {
-        this.graph = new LinkedList<Integer>[N];
+        this.graph = new ArrayList<LinkedList>(N);
     }
 
     public void addEdge(int from, int to) {
         // check if already exists
-        if (!graph[from-1].contains((Integer)(to-1))
-            graph[from-1].add(to-1);    // convert from natural to Java indexing
+        LinkedList fromVertex = graph.get(from-1);
+        if (!fromVertex.contains((Integer)(to-1)))
+            fromVertex.add(to-1);    // convert from natural to Java indexing
     }
 
     public void deleteEdge(int from, int to) {
         // check if exists
-        if (graph[from-1].contains((Integer)(to-1))
-            graph[from-1].remove(to-1); // convert from natural to Java indexing
+        LinkedList fromVertex = graph.get(from-1);
+        if (fromVertex.contains((Integer)(to-1)))
+            fromVertex.remove(to-1); // convert from natural to Java indexing
     }
 
     public int edgeCount() {
         int edges = 0;
-        for (int i = 0; i < graph.length; i++)
-            edges += graph[i].size();
+        for (int i = 0; i < graph.size(); i++)
+            edges += graph.get(i).size();
         return edges;
     }
 
     public int vertexCount() {
-        return graph.length;
+        return graph.size();
     }
 
     public void print() {
         int vertexes = vertexCount();
         for (int i = 0; i < vertexes; i++) {
             System.out.print((i + 1) + " is connected to : ");
-            int adjacent = graph[i].size();
-            for (int v = 0; v < adjacent - 1; v++) {
-                System.out.print((graph[i].get(v) + 1) + ", ");
+            LinkedList vertex = graph.get(i);
+            int adjacent = vertex.size();
+            for (int v = 0; v < adjacent; v++) {
+                System.out.print((vertex.get(v) + 1) + ", ");
             }
-            System.out.println(graph[i].get(v) + 1);
+            System.out.println(vertex.get(v) + 1);
         }
     }
 
--- DiGraphTest.java
+++ DiGraphTest.java
@@ -5,6 +5,9 @@
 * Part 1 & 2
 */
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class DiGraphTest {
     public void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,15 +17,33 @@ public class DiGraphTest {
 
         printMenu();
 
+        int from, to;
+
         switch(sc.next()) {
             case "a":
-                int from, to;
                 from = sc.nextInt();
                 to = sc.nextInt();
-                addEdge(from, to);
-                System.out.println(String.format("Edge:\n- From: %d\n- To: %d", from, to));
+                graph.addEdge(from, to);
+                System.out.println(String.format("Added Edge:\n- From: %d\n- To: %d", from, to));
+                break;
+            case "d":
+                from = sc.nextInt();
+                to = sc.nextInt();
+                graph.deleteEdge(from, to);
+                System.out.println(String.format("Deleted Edge:\n- From: %d\n- To: %d", from, to));
+                break;
+            case "e":
+                System.out.println("Number of edges is: " + graph.edgeCount());
+                break;
+            case "v":
+                System.out.println("Number of vertexes is: " + graph.vertexCount());
+                break;
+            case "p":
+                graph.print();
+                break;
+            default:
+                System.out.println("Invalid option.");
         }
-
     }
 
     private void printMenu() {
